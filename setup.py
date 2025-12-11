b8eebcf11e4ffce07fecfb9779debc7775f29b8f
Shreya-droid <Shreya-droid@users.noreply.github.com>
2025-12-11 17:33:08 +0530

    changes made

--- setup.py
+++ setup.py
@@ -7,10 +7,9 @@ setup(
     author_email='shreya_s.iitr.ac.in',
     description='A tool for SNP-based Poisson binding analysis on SNP-bind-n-seq sequencing data',
     license='MIT',
-    license_files=['LICENSE'],
     url='https://github.com/Shreya-droid/SNPoiss_bind_n_seq',
     py_modules=['run_all'],
-    packages=find_packages(include=['snpoiss_bind_n_seq', 'snpoiss_bind_n_seq.*']),
+    packages=find_packages(include=['ProBEX', 'ProBEX.*']),
     install_requires=['tensorflow'],
     entry_points={
         'console_scripts': [
